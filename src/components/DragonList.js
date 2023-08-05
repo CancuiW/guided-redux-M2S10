09465b9e69ce69fda94a1416041fbcb40cffb130
CancuiW <can.cui@wayne.edu>
2023-08-04 22:37:38 -0700

    finish

--- src/components/DragonList.js
+++ src/components/DragonList.js
@@ -1,38 +1,28 @@
 import React from 'react';
 import DragonMember from './DragonMember';
+import { connect } from 'react-redux';
+import { addMember,updateNewMember } from '../actions/dragonListActions';
 
 class DragonList extends React.Component {
-  state = {
-    newMember: '',
-    members: [
-      { name: 'Jojo Zhang', dragonStatus: true },
-      { name: 'Brandon Harris', dragonStatus: false }
-    ]
-  };
 
-  handleChanges = e => {
-    this.setState({ ...this.state, newMember: e.target.value });
-  };
+  handleClick=()=>{
+    this.props.addMember({name:this.props.newMember,dragonStatus:true})
 
-  handleClick = ()=> {
-    this.setState({
-      ...this.state,
-      members: [...this.members, {name: newMember, dragonStatus: true}]
-    })
+
   }
-
+  
   render() {
     return (
       <div>
         <div className="friends-list">
-          {this.state.members.map((member, index) => (
+          {this.props.members.map((member, index) => (
             <DragonMember key={index} member={member}/>
           ))}
         </div>
         <input
           type="text"
-          value={this.state.newMember}
-          onChange={this.handleChanges}
+          value={this.props.newMember}
+          onChange={(e)=>{this.props.updateNewMember(e.target.value)}}
           placeholder="Add new member"
         />
         <button onClick={this.handleClick}>Add member</button>
@@ -40,5 +30,11 @@ class DragonList extends React.Component {
     );
   }
 }
+const mapStateToProps = state => {
+  return {
+    members:state.dragonList.members,
+    newMember: state.dragonList.newMember
+  }
+}
 
-export default DragonList;
+export default connect(mapStateToProps,{addMember,updateNewMember})(DragonList);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { legacy_createStore as createStore } from 'redux' ;
+import { Provider } from 'react-redux';
+import { rootReducer } from './reducers';
 
 import Title from './components/Title';
 import DragonList from './components/DragonList';
 import './styles.css';
+//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//this code means if 'window.__REDUX_DEVTOOLS_EXTENSION__ 'is true,we can use 
+//'window.__REDUX_DEVTOOLS_EXTENSION__()' to see the Redux store which displays on the
+//google wabsite.
+
+const store=createStore(rootReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 function App() {
   return (
@@ -15,4 +24,9 @@ function App() {
 }
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+
+, rootElement);
